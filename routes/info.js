129d07f3ffc9de8cbd6244c0888534b616c2cca6
Tom French <tom@tomfren.ch>
2019-05-19 16:04:31 +0100

    Add: adds extra information to info route

--- routes/info.js
+++ routes/info.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
+var mongoose = require('mongoose')
 
 router.route('/')
   .get((req, res) => {
-    const response = { version: '0.2.0' }
+    const response = {
+      api_version: '0.2.0',
+      db_url: process.env.DATABASE_HOST,
+      connected_to_db: mongoose.connection.readyState
+    }
     res.json(response)
   })
 
